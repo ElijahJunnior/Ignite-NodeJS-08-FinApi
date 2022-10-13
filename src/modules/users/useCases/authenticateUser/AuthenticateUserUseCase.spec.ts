b6bf5554d22c65dedc06ed4522e8efbad74a66b0
Elias Junior <elijah.junnior@outlook.com>
2022-10-13 09:05:12 -0300

    creation of the authenticate user use case tests

---
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,81 @@
+import { hash } from "bcryptjs";
+import { AppError } from "../../../../shared/errors/AppError";
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { IUsersRepository } from "../../repositories/IUsersRepository"
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+let usersRepository: IUsersRepository;
+let authenticateUserUseCase: AuthenticateUserUseCase;
+
+describe("Authenticate User Use Case", () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    authenticateUserUseCase = new AuthenticateUserUseCase(usersRepository);
+  })
+
+  it("Should be able to authenticate user", async () => {
+    const email = "test.user@test.com";
+    const password = "password123"
+    const password_hash = await hash(password, 8);
+
+    await usersRepository.create({
+      email,
+      name: "Test User",
+      password: password_hash,
+    });
+
+    const auth = await authenticateUserUseCase.execute({
+      email, password
+    })
+
+    expect(auth).toHaveProperty("token");
+  })
+
+  it("Should not be able to authenticate user with incorrect email", async () => {
+    let error;
+
+    try {
+      const password = "password123";
+      const password_hash = await hash(password, 8);
+
+      await usersRepository.create({
+        email: "test.user@test.com",
+        name: "Test User",
+        password: password_hash
+      })
+
+      await authenticateUserUseCase.execute({
+        email: "incorrect@test.com",
+        password
+      });
+    } catch(err) {
+      error = err;
+    }
+
+    expect(error).toEqual(new AppError("Incorrect email or password", 401));
+  })
+
+  it("Should not be able to authenticate user with incorrect password", async () => {
+    let error;
+
+    try {
+      const password_hash = await hash("password123", 8);
+      const email = "test.user@test.com";
+
+      await usersRepository.create({
+        email,
+        name: "Test User",
+        password: password_hash
+      })
+
+      await authenticateUserUseCase.execute({
+        email,
+        password: "incorrect"
+      });
+    } catch(err) {
+      error = err;
+    }
+
+    expect(error).toEqual(new AppError("Incorrect email or password", 401));
+  })
+})
