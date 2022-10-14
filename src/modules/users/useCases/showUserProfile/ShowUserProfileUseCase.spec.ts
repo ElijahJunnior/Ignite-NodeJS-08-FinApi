0313be1d20ecfb252fd94c851a13f7ca53790865
Elias Junior <elijah.junnior@outlook.com>
2022-10-14 06:56:38 -0300

    creation of show user use case test

---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
@@ -0,0 +1,40 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { IUsersRepository } from "../../repositories/IUsersRepository"
+import { ShowUserProfileError } from "./ShowUserProfileError";
+import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
+
+let usersRepository: IUsersRepository;
+let showUserProfileUseCase: ShowUserProfileUseCase;
+
+describe("Show User Profile Use Case", () => {
+  beforeEach(()=> {
+    usersRepository = new InMemoryUsersRepository();
+    showUserProfileUseCase = new ShowUserProfileUseCase(usersRepository);
+  })
+
+  it("Should be able to return the data of an existing user", async () => {
+    const userCreated = await usersRepository.create({
+      email: "user.test@finapi.com",
+      name: "User Test",
+      password: "password123"
+    });
+
+    const userReturned = await showUserProfileUseCase.execute(
+      userCreated.id as string
+    );
+
+    expect(userReturned).toEqual(userCreated);
+  })
+
+  it("shouldn't be able to return data from a user that doesn't exist", async () => {
+    let error;
+
+    try {
+      await showUserProfileUseCase.execute("not_exists_user_id");
+    } catch (err) {
+      error = err;
+    }
+
+    expect(error).toEqual(new ShowUserProfileError())
+  })
+})
