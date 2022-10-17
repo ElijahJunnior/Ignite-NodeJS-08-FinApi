c1f87582826d1c6051535251a90b7b98483f9c8a
Elias Junior <elijah.junnior@outlook.com>
2022-10-17 19:57:37 -0300

    creation of the Create User Controller tests

--- src/modules/users/useCases/createUser/CreateUserController.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserController.spec.ts
@@ -1,7 +1,54 @@
+import request from "supertest";
+import { Connection } from "typeorm";
 
+import { app } from "../../../../app";
+import { createDatabaseConnection } from "../../../../database";
+
+let connection: Connection;
 
 describe("Create User Controller", () => {
+  beforeAll(async () => {
+    connection = await createDatabaseConnection();
+    await connection.runMigrations();
+  })
 
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close()
+  })
 
+  it("Should be able to create a new user", async () => {
+    const res = await request(app)
+      .post("/api/v1/users")
+      .send({
+        name: "User Test",
+        password: "password123",
+        email: "user.test@fin_api.com"
+      });
 
+    expect(res.status).toEqual(201);
+  })
+
+  it("Shouldn't be able to create two users with the same email", async () => {
+    await request(app)
+      .post("/api/v1/users")
+      .send({
+        name: "User Failure Test",
+        password: "password123",
+        email: "user.failure.test@fin_api.com"
+      });
+
+    const res = await request(app)
+      .post("/api/v1/users")
+      .send({
+        name: "User Failure Test",
+        password: "password1234",
+        email: "user.failure.test@fin_api.com"
+      });
+
+    expect(res.status).toEqual(400);
+    expect(res.body).toEqual({
+      message: "User already exists"
+    });
+  })
 })
