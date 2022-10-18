8ab6445361c61d401d1f34f71f3c8a823ee95d16
Elias Junior <elijah.junnior@outlook.com>
2022-10-18 05:09:46 -0300

    creation the rest of the show user profile controller tests

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.spec.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.spec.ts
@@ -1,4 +1,4 @@
-import req from "supertest";
+import request from "supertest";
 import { Connection } from "typeorm";
 
 import { app } from "../../../../app";
@@ -15,7 +15,7 @@ describe("Show User Profile Controller", () => {
   afterAll(async () => {
     await connection.dropDatabase();
     await connection.close();
-  })
+  });
 
   it("Should be able to get user data", async () => {
 
@@ -23,7 +23,7 @@ describe("Show User Profile Controller", () => {
     const password = "password123";
     const email = "user.test@fin_api.com";
 
-    const resCreateUser = await req(app)
+    const resCreateUser = await request(app)
       .post("/api/v1/users")
       .send({
         email,
@@ -33,23 +33,23 @@ describe("Show User Profile Controller", () => {
 
     expect(resCreateUser.status).toEqual(201);
 
-    const resCreateSession = await req(app)
+    const resCreateSession = await request(app)
       .post("/api/v1/sessions")
       .send({
         email,
         password
-      })
+      });
 
     expect(resCreateSession.status).toEqual(200);
     expect(resCreateSession.body).toHaveProperty("token");
 
     const { token } = resCreateSession.body;
 
-    const resShowUser = await req(app)
+    const resShowUser = await request(app)
       .get("/api/v1/profile")
       .set({
         Authorization: `Bearer ${token}`
-      })
+      });
 
     expect(resShowUser.status).toEqual(200);
 
@@ -59,5 +59,27 @@ describe("Show User Profile Controller", () => {
 
     expect(resShowUser.body.name).toEqual(name);
     expect(resShowUser.body.email).toEqual(email);
+  })
+
+  it("Shouldn't be able to get user date without token", async () => {
+    const response = await request(app).get("/api/v1/profile");
+
+    expect(response.status).toEqual(401);
+    expect(response.body).toEqual({
+      message: 'JWT token is missing!'
+    });
+  })
+
+  it("Shouldn't be able to get user date with incorrect token", async () => {
+    const response = await request(app)
+      .get("/api/v1/profile")
+      .set({
+        Authorization: `Bearer INCORRECT_TOKEN`
+      });
+
+    expect(response.status).toEqual(401);
+    expect(response.body).toEqual({
+      message: 'JWT invalid token!'
+    })
   })
 })
