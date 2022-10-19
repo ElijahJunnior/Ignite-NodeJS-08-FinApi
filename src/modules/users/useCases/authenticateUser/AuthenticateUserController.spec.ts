c5a58b4fc3a55eed64068c5dc0deb59a02d06161
Elias Junior <elijah.junnior@outlook.com>
2022-10-19 08:33:22 -0300

    creating the rest of the authenticate user use case integration tests

--- src/modules/users/useCases/authenticateUser/AuthenticateUserController.spec.ts
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserController.spec.ts
@@ -12,13 +12,17 @@ describe("Authenticate User Controller", () => {
     await connection.runMigrations();
   })
 
+  beforeEach(async () => {
+    await connection.query("DELETE FROM users");
+  })
+
   afterAll(async () => {
     await connection.dropDatabase();
     await connection.close()
   })
 
   it("Should be able to create a session", async () => {
-    const email = "user.test@email.com";
+    const email = "user.test@fin_api.com";
     const password = "password123";
     const name = "User Test";
 
@@ -43,5 +47,55 @@ describe("Authenticate User Controller", () => {
     expect(resCreateSession.body).toHaveProperty("token");
     expect(resCreateSession.body).toHaveProperty("user");
     expect(resCreateSession.body.user.name).toEqual(name);
+  })
+
+  it("Shouldn't be able to create user with incorrect email", async () => {
+    const email = "user.test@fin_api.com";
+    const password = "password123";
+
+    const resCreateUser = await request(app)
+      .post("/api/v1/users")
+      .send({
+        name: "User Test",
+        email,
+        password
+      });
+
+    expect(resCreateUser.status).toBe(201);
+
+    const resCreateSession = await request(app)
+      .post("/api/v1/sessions")
+      .send({
+        email: "incorrect@fin_api.com",
+        password
+      });
+
+    expect(resCreateSession.status).toBe(401);
+    expect(resCreateSession.body).toEqual({message: "Incorrect email or password"});
+  })
+
+  it("Shouldn't be able to create user with incorrect password", async () => {
+    const email = "user.test@fin_api.com";
+    const password = "password123";
+
+    const resCreateUser = await request(app)
+      .post("/api/v1/users")
+      .send({
+        name: "User Test",
+        email,
+        password
+      });
+
+    expect(resCreateUser.status).toBe(201);
+
+    const resCreateSession = await request(app)
+      .post("/api/v1/sessions")
+      .send({
+        email,
+        password: "incorrectPassword123"
+      });
+
+    expect(resCreateSession.status).toBe(401);
+    expect(resCreateSession.body).toEqual({message: "Incorrect email or password"});
   })
 })
