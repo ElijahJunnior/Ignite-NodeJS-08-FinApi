9f610457702f6eeb13d8ad4904363e1cf89772e2
Elias Junior <elijah.junnior@outlook.com>
2022-10-20 12:37:02 -0300

    creation the rest of the create statement controller tests

--- src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
@@ -38,6 +38,10 @@ describe("Create Statement Controller", () => {
 
   })
 
+  afterEach(async() => {
+    await connection.query("DELETE FROM statements");
+  })
+
   afterAll(async () => {
     await connection.dropDatabase();
     await connection.close();
@@ -56,5 +60,79 @@ describe("Create Statement Controller", () => {
 
     expect(response.status).toBe(201);
     expect(response.body).toHaveProperty("id");
+  })
+
+  it("Should be able to create a withdraw statement", async () => {
+    await request(app)
+      .post("/api/v1/statements/deposit")
+      .send({
+        amount: 120.00,
+        description: "Test deposit statement"
+      })
+      .set({
+        Authorization: `Bearer ${token}`
+      });
+
+    const response = await request(app)
+      .post("/api/v1/statements/withdraw")
+      .send({
+        amount: 120.00,
+        description: "Test withdraw statement"
+      })
+      .set({
+        Authorization: `Bearer ${token}`
+      });
+
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty("id");
+  })
+
+  it("Shouldn't be able to create a deposit statement without token", async () => {
+    const response = await request(app)
+      .post("/api/v1/statements/deposit")
+      .send({
+        amount: 120.00,
+        description: "Test deposit statement"
+      });
+
+    expect(response.status).toBe(401);
+    expect(response.body).toEqual({message: "JWT token is missing!"});
+  })
+
+  it("Shouldn't be able to create a withdraw statement without token", async () => {
+    const response = await request(app)
+      .post("/api/v1/statements/withdraw")
+      .send({
+        amount: 120.00,
+        description: "Test withdraw statement"
+      });
+
+    expect(response.status).toBe(401);
+    expect(response.body).toEqual({message: "JWT token is missing!"});
+  })
+
+  it("Shouldn't be able to create a withdraw statement without sufficient funds", async () => {
+    await request(app)
+      .post("/api/v1/statements/deposit")
+      .send({
+        amount: 100.00,
+        description: "Test deposit statement"
+      })
+      .set({
+        Authorization: `Bearer ${token}`
+      });
+
+    const response = await request(app)
+      .post("/api/v1/statements/withdraw")
+      .send({
+        amount: 120.00,
+        description: "Test withdraw statement"
+      })
+      .set({
+        Authorization: `Bearer ${token}`
+      });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({message: "Insufficient funds"});
   })
 })
